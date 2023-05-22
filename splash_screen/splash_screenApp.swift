8f635411297925a7c26bb751ec40c20c75eb0507
quintus <john@chaordo.com>
2023-05-22 12:15:59 -0600

    Initial Commit

---
+++ splash_screen/splash_screenApp.swift
@@ -0,0 +1,17 @@
+//
+//  splash_screenApp.swift
+//  splash_screen
+//
+//  Created by John on 5/22/23.
+//
+
+import SwiftUI
+
+@main
+struct splash_screenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
